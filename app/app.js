8da0d8d09181db272c050dc5af9ff7c2576a059b
kholofeloj <kholofelojmocheko@gmail.com>
2025-04-04 19:05:42 +0200

    Responsive nav bar

--- app/app.js
+++ app/app.js
@@ -1,1 +1,5 @@
+
+
+// this function gets the current year and sets it in the footer
+// it is used to avoid having to update the year manually
 document.getElementById("currentYear").textContent = new Date().getFullYear();
