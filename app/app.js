820d9920d823d24ae1d26ddc2a14e91022168d40
kholofeloj <kholofelojmocheko@gmail.com>
2025-04-04 20:45:52 +0200

    final touches

--- app/app.js
+++ app/app.js
@@ -1,4 +1,30 @@
+// smooth scrolling
+const links = document.querySelectorAll(".nav-bar li a");
 
+for (const link of links) {
+  link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(event) {
+    event.preventDefault();
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+    });
+}
+
+function showSidebar() {
+  // this function shows the sidebar
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex"
+}
+function hideSidebar() {
+  // this function hides the sidebar
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none"
+}
 
 // this function gets the current year and sets it in the footer
 // it is used to avoid having to update the year manually
